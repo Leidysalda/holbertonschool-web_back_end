a6d84a9b8a1da99ca9d13b5209c6d83a103c92b5
Leidysalda <leidysalda1@gmail.com>
2021-07-21 02:39:23 +0000

    [Fix] python style pep8

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,20 +4,20 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int=2) -> List:
     """
-    Use mypy to validate the following piece of code and apply any 
+    Use mypy to validate the following piece of code and apply any
     necessary changes.
     """
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-    array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
-    zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-    zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3.0)
