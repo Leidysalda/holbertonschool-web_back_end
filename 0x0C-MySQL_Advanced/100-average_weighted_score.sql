9e4361c470e83ad7bfef18441f5ca91dff74df86
leidysalda <leidysalda1@gmail.com>
2021-09-03 03:11:40 +0000

    [Fix file 100-]

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -2,10 +2,10 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
 DELIMITER |
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
 DECLARE average FLOAT;
-SET average = (SELECT (SUM(score * weight)/SUM(weight)) FROM corrections AS newTable INNER JOIN projects ON projects_id = id WHERE newTable.user_id = user_id);
+SET average = (SELECT (SUM(score * weight)/SUM(weight)) FROM corrections AS newTable INNER JOIN projects ON project_id = id WHERE newTable.user_id=user_id);
 UPDATE users SET average_score=average WHERE id=user_id;
 
 END;
