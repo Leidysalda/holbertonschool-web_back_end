b990e39a5fd613ae268d075c1fe72548265d5fa9
leidysalda <leidysalda1@gmail.com>
2021-09-03 02:50:57 +0000

    [Fix]file 3-

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- 3. Old school band
 -- SQL script that lists all bands with Glam rock as their main style
-SELECT bans_name, (IFNULL(split, 2020)-formed) AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2020)-formed) AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
