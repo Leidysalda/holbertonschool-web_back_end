d3d76b643cb5005bef3172e68e9e9186874176d5
leidysalda <leidysalda1@gmail.com>
2021-09-03 02:48:45 +0000

    [Fix] file 0

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- 0. We are all unique
 -- SQL script that creates a table users
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255)
