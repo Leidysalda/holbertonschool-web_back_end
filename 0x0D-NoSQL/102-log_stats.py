fe686e7c67d9110033101e3da9340f1ad3c78e00
leidysalda <leidysalda1@gmail.com>
2021-09-08 02:22:00 +0000

    [Fix]file 102-log_stats

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -10,34 +10,32 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print("{} logs".format(collection.estimated_document_count()))
+    print(f"{collection.estimated_document_count()} logs")
 
     print("Methods:")
 
-    for method in ["GET". "POST", "PUT", "PATCH", "DELETE"]:
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         method_count = collection.count_documents({'method': method})
-        print(f"\tmethod {method}: {method_Count}")
+        print(f"\tmethod {method}: {method_count}")
 
     check_get = collection.count_documents(
         {'method': 'GET', 'path': "/status"})
     print(f"{check_get} status check")
 
     print("IPs:")
-    top_ips = collections.aggregate ([
+    top_ips = collection.aggregate([
         {"$group":
-            {
-                "_id": "$ip",
-                "count": {"$sum": 1}
-            }
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
         },
         {"$sort": {"count": -1}},
         {"$limit": 10},
-        {"$project": {
+        {"$prohject": {
             "_id": 0,
-            "ip": "$_id",
             "count": 1
-        }} 
+        }}
     ])
-
     for ip in top_ips:
-        print(f"\t {ip.get('ip')}: {ip.get('count')}")
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
