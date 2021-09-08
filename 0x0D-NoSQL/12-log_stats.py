3e8fbd09ac1e35dbd50aaf5366b12945e3b29b37
leidysalda <leidysalda1@gmail.com>
2021-09-08 01:50:47 +0000

    [Fix] file 12-log_stats

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -7,15 +7,15 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """ stats about Nginx logs stored in MongoDB
     """
-    client = MOngoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
     print("{} logs".format(collection.estimated_document_count()))
 
     print("Methods:")
 
-    for method in ["GET". "POST", "PUT", "PATCH", "DELETE"]:
-        method_count = collection.count_documents({'methos': method})
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
         print("\t method {}: {}".format(method, method_count))
 
     check_get = collection.count_documents(
