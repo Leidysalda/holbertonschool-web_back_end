417a44ca6c2ac4a0c54181b1ac86d2d3e824d52e
leidysalda <leidysalda1@gmail.com>
2021-09-15 00:18:20 -0500

    [Fix] file

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
-export default function getResponseFromAPI(success) {
-    if (!success) return Promise.reject(new Error('The fake API is not working currently'));
-    return Promise.resolve({
-        status: 200,
-        body: 'success',
-    });
+export default function getFullResponseFromAPI(success) {
+  if (!success) return Promise.reject(new Error('The fake API is not working currently'));
+  return Promise.resolve({
+    status: 200,
+    body: 'Success',
+  });
 }
