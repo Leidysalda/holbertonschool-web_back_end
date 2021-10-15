650a812a4dc8ccef8eade325aae3183175adac00
Leidysalda <leidysalda1@gmail.com>
2021-10-15 03:49:52 +0000

    [Fix] files

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -16,7 +16,7 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """put method"""
-        if key is None and item is None:
+        if key is None or item is None:
             return
 
         self.cache_data[key] = item
