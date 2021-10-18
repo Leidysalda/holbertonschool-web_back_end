bc9d0915a2909fec56ae57321e1c0ccee8473303
Leidysalda <leidysalda1@gmail.com>
2021-10-18 05:01:44 +0000

    [Fix] file

--- 0x0E-ES6_basic/6-string-interpolation.js
+++ 0x0E-ES6_basic/6-string-interpolation.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const getSanFranciscoDescription = () => {
   const year = 2017;
   const budget = {
@@ -8,7 +7,7 @@ const getSanFranciscoDescription = () => {
     capita: '$178,479',
   };
 
-  return 'As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp} and a GDP per capita of ${budget.capita}.';
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp} and a GDP per capita of ${budget.capita}.`;
 };
 
 export default getSanFranciscoDescription;
