6e903def00e2fcae245d23d8d68a7b8474306500
Leidysalda <leidysalda1@gmail.com>
2021-10-18 04:59:20 +0000

    [Fix] file

--- 0x0E-ES6_basic/5-spread-operator.js
+++ 0x0E-ES6_basic/5-spread-operator.js
@@ -1,7 +1,6 @@
-
 const concatArrays = (array1, array2, string) => {
   const arr = [];
-  arr.push(...array1, ...array2,...string);
+  arr.push(...array1, ...array2, ...string);
   return arr;
 };
 
